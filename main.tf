82387fae96638f0dc9aa8c07d6c748c135cc9da5
Ayush Nigam <ayushnigam0001@gmail.com>
2023-12-23 10:01:30 +0530

    init commit

---
+++ main.tf
@@ -0,0 +1,21 @@
+module "server_01" {
+  source            = "git::https://github.com/ayushnigam0001/terraformmodules.git//aws_ec2"
+  server_name       = "Server01"
+  availability_zone = "ap-south-1a"
+  root_volume_size  = 11
+  ebs_block_devices = [
+    {
+      "device_name" = "/dev/sdb"
+      "volume_size" = 3
+    },
+    {
+      "device_name" = "/dev/sdc"
+      "volume_size" = 3
+    }
+  ]
+  ami_id              = "ami-04708942c263d8190"
+  instance_type       = "t2.micro"
+  subnet_id           = "subnet-00848b73f86a37141"
+  key                 = "pem-key"
+  security_group_list = ["sg-063bfb276ea028c07"]
+}
