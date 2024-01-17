c9a0bd6cf65ffffc9bba9fd85b0ae9a3eb3ed7df
Ayush Nigam <ayushnigam0001@gmail.com>
2024-01-17 07:32:43 +0530

    deleted ebs

--- ebs-vol01.tf
+++
@@ -1,8 +0,0 @@
-module "ebs_vol01" {
-  source = "git::https://github.com/gareeb-engineer/terraform-modules.git?ref=v1.0.3"
-  availability_zone = "ap-south-1a"
-  volume_size = 12
-  regular_tag = {
-    "Name" = "EBS-VOl01"
-  }
-}
