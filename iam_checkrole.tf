74e00ab6d3cd24475b1d8c701851f39058f15a91
Ayush Nigam <ayushnigam0001@gmail.com>
2024-05-13 00:48:45 +0530

    check2

--- iam_checkrole.tf
+++ iam_checkrole.tf
@@ -1,6 +1,6 @@
 data "aws_iam_users" "all_users" {
 }
 
-output "iam_users" {
-  value = data.aws_iam_users.all_users.users[*].name
+output "iam_user_arns" {
+  value = data.aws_iam_users.all_users.ids
 }
